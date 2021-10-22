28ad6b5db04f15236dd7a0a0ee7ea05998d40b7b
Dona Ugalde <dona.ugalde@gmail.com>
2021-10-21 22:40:11 -0500

    java-8-initial-commit
    Hello world Java 8 with SpringBoot

---
+++ java/carrer-path-java-8/src/main/java/donaugalde/careerpath/java8/CarrerPathJava8Application.java
@@ -0,0 +1,20 @@
+package donaugalde.careerpath.java8;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerPathJava8Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrerPathJava8Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("dup - Hello world Java 8 with SpringBoot!");
+	}
+
+}
---
+++ java/carrer-path-java-8/src/test/java/donaugalde/careerpath/java8/CarrerPathJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package donaugalde.careerpath.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerPathJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
