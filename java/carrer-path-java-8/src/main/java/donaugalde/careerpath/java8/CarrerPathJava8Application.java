cb42a127cf9add30c28b833a5a9a7ce14e137256
Donaciano Ugalde <dona.ugalde@gmail.com>
2021-10-21 23:04:16 -0500

    java-8-comments-and-git-commits
    Java comments and git commits

--- java/carrer-path-java-8/src/main/java/donaugalde/careerpath/java8/CarrerPathJava8Application.java
+++ java/carrer-path-java-8/src/main/java/donaugalde/careerpath/java8/CarrerPathJava8Application.java
@@ -5,6 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * Sample class with main method in SpringBoot.
+ * 
+ * @author donaugalde
+ *
+ */
 @SpringBootApplication
 public class CarrerPathJava8Application implements CommandLineRunner {
 
